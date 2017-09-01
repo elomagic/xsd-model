59d28bf40e49b546b96b6bc4bfa18afc3f2a276a
Carsten Rambow <developer@elomagic.de>
2017-09-01 08:23:07 +0200

    Package info classes added

--- src/main/java/de/elomagic/xsdmodel/elements/package-info.java
+++ src/main/java/de/elomagic/xsdmodel/elements/package-info.java
@@ -1,7 +1,7 @@
 /*
- * Contains the implementation of the XSD model.
+ * Contains the interfaces of the XSD model.
  * <p>
- * It's advisable that your application don't to use these classes to avoid issues in the future by refactoring etc.
+ * Usually, your application will use these classes.
  */
 @XmlSchema(
         namespace = "http://www.w3.org/2001/XMLSchema",
@@ -10,7 +10,7 @@
             @XmlNs(prefix = "xs", namespaceURI = "http://www.w3.org/2001/XMLSchema"),
             @XmlNs(prefix = "xml", namespaceURI = "http://www.w3.org/XML/1998/namespace")
         })
-package de.elomagic.xsdmodel.elements.impl;
+package de.elomagic.xsdmodel.elements;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
