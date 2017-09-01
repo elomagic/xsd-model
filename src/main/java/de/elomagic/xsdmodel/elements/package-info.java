a2743ec97bd942db2cf5c4ba06a06602441bc8fe
Carsten Rambow <developer@elomagic.de>
2017-09-01 08:59:56 +0200

    Xml annotations removed

--- src/main/java/de/elomagic/xsdmodel/elements/package-info.java
+++ src/main/java/de/elomagic/xsdmodel/elements/package-info.java
@@ -3,15 +3,4 @@
  * <p>
  * Usually, your application will use these classes.
  */
-@XmlSchema(
-        namespace = "http://www.w3.org/2001/XMLSchema",
-        elementFormDefault = XmlNsForm.QUALIFIED,
-        xmlns = {
-            @XmlNs(prefix = "xs", namespaceURI = "http://www.w3.org/2001/XMLSchema"),
-            @XmlNs(prefix = "xml", namespaceURI = "http://www.w3.org/XML/1998/namespace")
-        })
 package de.elomagic.xsdmodel.elements;
-
-import javax.xml.bind.annotation.XmlNs;
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
