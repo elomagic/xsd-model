85e28e2c5e16af2688b588b8975bc7139455864e
Carsten Rambow <dev@elomagic.de>
2023-07-31 11:16:20 +0200

    Methods "hashCode" and "equals" added

--- src/main/java/de/elomagic/xsdmodel/converter/KeyProperties.java
+++ src/main/java/de/elomagic/xsdmodel/converter/KeyProperties.java
@@ -1,3 +1,20 @@
+/*
+ * XSD Model (Java 17 with Jakarta XML Binding 4.0)
+ * Copyright (c) 2017-present Carsten Rambow
+ * mailto:developer AT elomagic DOT de
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.elomagic.xsdmodel.converter;
 
 public class KeyProperties {
@@ -37,6 +54,21 @@ public class KeyProperties {
 
     public void setDefaultValue(String defaultValue) {
         this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        KeyProperties that = (KeyProperties) o;
+
+        return key.equals(that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode();
     }
 
 }
